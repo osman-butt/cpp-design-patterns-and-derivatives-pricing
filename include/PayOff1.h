c7703bed29678497c965d4395d5e1f2a5d8e88d7
osman-butt <butt.osman@gmail.com>
2025-01-17 17:25:32 +0100

    Added comments

--- include/PayOff1.h
+++ include/PayOff1.h
@@ -5,9 +5,14 @@ class PayOff
 public:
 	enum OptionType { call, put };
 	PayOff(double Strike_, OptionType TheOptionsType_);
-	double operator()(double Spot) const;
+	double operator()(double Spot) const; // Overloaded operator() method
 private:
 	double Strike;
 	OptionType TheOptionsType;
 };
 #endif
+
+// The overloaded operator() method is a function call operator.
+// It is a special function that is called when an object of the class is called as if it were a function.
+// The const is a convinience to the user of the class.
+// It tells that the Strike or option type would not be changed by the function.
--- src/PayOff1.cpp
+++ src/PayOff1.cpp
@@ -1,11 +1,20 @@
 #include "../include/PayOff1.h"
 #include <MinMax.h>
 
+
+// The constructor initializes the Strike and TheOptionsType member variables,
+// by using the initializer list syntax.
+// This has the advantage of being more efficient than assigning 
+// values to the member variables in the constructor body.
 PayOff::PayOff(double Strike_, OptionType TheOptionsType_)
 	: Strike(Strike_), TheOptionsType(TheOptionsType_)
 {
 }
 
+// The operator() method is a function call operator. 
+// It is a special function that is called when an object of the class is called as if it were a function.
+// The const is a convinience to the user of the class.
+// It tells that the Strike or option type would not be changed by the function.
 double PayOff::operator()(double spot) const {
 	switch (TheOptionsType)
 	{
