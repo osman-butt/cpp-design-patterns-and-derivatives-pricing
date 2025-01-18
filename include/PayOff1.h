217aff7f2807af68a2fa8b0aba36a9a11baa25f4
osman-butt <butt.osman@gmail.com>
2025-01-18 13:47:34 +0100

    Added comments

--- include/PayOff1.h
+++ include/PayOff1.h
@@ -18,3 +18,9 @@ private:
 // It is a special function that is called when an object of the class is called as if it were a function.
 // The const is a convinience to the user of the class.
 // It tells that the Strike or option type would not be changed by the function.
+
+// Adding new option types:
+// If we want to price a digital option, we would need to add to the enum,
+// and add to the switch statement. If we did this, we would need to recompile
+// every file that uses the PayOff class => THE CODE IS NOT INDEPENDENT
+// From SOLID principles we should adapt the open-closed principles.
