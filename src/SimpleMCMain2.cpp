57dc631153a59c9ff48a4a74a71bd3018536cc48
osman-butt <butt.osman@gmail.com>
2025-01-20 19:43:25 +0100

    Uncomment main fct

--- src/SimpleMCMain2.cpp
+++ src/SimpleMCMain2.cpp
@@ -2,60 +2,60 @@
 #include "../include/SimpleMC.h"
 using namespace std;
 
-int main()
-{
-	double Expiry;
-	double Strike;
-	double Spot;
-	double Vol;
-	double r;
-	unsigned long NumberOfPaths;
-	cout << "\nEnter expiry\n";
-	cin >> Expiry;
-	cout << "\nEnter strike\n";
-	cin >> Strike;
-	cout << "\nEnter spot\n";
-	cin >> Spot;
-	cout << "\nEnter vol\n";
-	cin >> Vol;
-	cout << "\nr\n";
-	cin >> r;
-	cout << "\nNumber of paths\n";
-	cin >> NumberOfPaths;
-	PayOff callPayOff(Strike, PayOff::call);
-	PayOff putPayOff(Strike, PayOff::put);
-	PayOff digitalCallPayOff(Strike, PayOff::digitalCall);
-	PayOff digitalPutPayOff(Strike, PayOff::digitalPut);
-	double resultCall = SimpleMonteCarlo2(callPayOff,
-		Expiry,
-		Spot,
-		Vol,
-		r,
-		NumberOfPaths);
-	double resultPut = SimpleMonteCarlo2(putPayOff,
-		Expiry,
-		Spot,
-		Vol,
-		r,
-		NumberOfPaths);
-	double resultDigitalCall = SimpleMonteCarlo2(digitalCallPayOff,
-		Expiry,
-		Spot,
-		Vol,
-		r,
-		NumberOfPaths);
-	double resultDigitalPut = SimpleMonteCarlo2(digitalPutPayOff,
-		Expiry,
-		Spot,
-		Vol,
-		r,
-		NumberOfPaths);
-	cout << "Prices:\n";
-	cout << "Call: " << resultCall << "\n";
-	cout << "Put: " << resultPut << "\n";
-	cout << "Digital Call: " << resultDigitalCall << "\n";
-	cout << "Digital Put: " << resultDigitalPut << "\n";
-	double tmp;
-	cin >> tmp;
-	return 0;
-}
+//int main()
+//{
+//	double Expiry;
+//	double Strike;
+//	double Spot;
+//	double Vol;
+//	double r;
+//	unsigned long NumberOfPaths;
+//	cout << "\nEnter expiry\n";
+//	cin >> Expiry;
+//	cout << "\nEnter strike\n";
+//	cin >> Strike;
+//	cout << "\nEnter spot\n";
+//	cin >> Spot;
+//	cout << "\nEnter vol\n";
+//	cin >> Vol;
+//	cout << "\nr\n";
+//	cin >> r;
+//	cout << "\nNumber of paths\n";
+//	cin >> NumberOfPaths;
+//	PayOff callPayOff(Strike, PayOff::call);
+//	PayOff putPayOff(Strike, PayOff::put);
+//	PayOff digitalCallPayOff(Strike, PayOff::digitalCall);
+//	PayOff digitalPutPayOff(Strike, PayOff::digitalPut);
+//	double resultCall = SimpleMonteCarlo2(callPayOff,
+//		Expiry,
+//		Spot,
+//		Vol,
+//		r,
+//		NumberOfPaths);
+//	double resultPut = SimpleMonteCarlo2(putPayOff,
+//		Expiry,
+//		Spot,
+//		Vol,
+//		r,
+//		NumberOfPaths);
+//	double resultDigitalCall = SimpleMonteCarlo2(digitalCallPayOff,
+//		Expiry,
+//		Spot,
+//		Vol,
+//		r,
+//		NumberOfPaths);
+//	double resultDigitalPut = SimpleMonteCarlo2(digitalPutPayOff,
+//		Expiry,
+//		Spot,
+//		Vol,
+//		r,
+//		NumberOfPaths);
+//	cout << "Prices:\n";
+//	cout << "Call: " << resultCall << "\n";
+//	cout << "Put: " << resultPut << "\n";
+//	cout << "Digital Call: " << resultDigitalCall << "\n";
+//	cout << "Digital Put: " << resultDigitalPut << "\n";
+//	double tmp;
+//	cin >> tmp;
+//	return 0;
+//}
